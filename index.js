9f6d2800e8b491780d01724c7f0e87fbb3d6f2cb
golden <amdexec@gmail.com>
2023-09-08 17:22:27 +0330

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,39 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-const app = require('./api/app');
+
+const express = require('express');
+const morgan = require('morgan');
+
+const seriesRouter = require('./routes/seriesRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTE HANDLERS
+
+// 3) ROUTES
+
+app.use('/api/v1/series', seriesRouter);
 
 // const DB = process.env.DATABASE.replace(
 //   '<PASSWORD>',
