16746d960d7c998a48100d880bb9f5d1ca8bf820
golden <amdexec@gmail.com>
2023-09-08 16:59:23 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const seriesRouter = require('./routes/seriesRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTE HANDLERS
+
+// 3) ROUTES
+
+app.use('/api/v1/series', seriesRouter);
+
+module.exports = app;
